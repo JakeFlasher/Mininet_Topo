21a0426e9fb17640e076655c801c1d170ca35649
JakeFlasher <jakeflasher@outlook.com>
2020-03-27 16:22:23 +0800

    im

--- FatTree/fattree.py
+++ FatTree/fattree.py
@@ -8,158 +8,160 @@ from mininet.link import Link, Intf, TCLink
 from mininet.topo import Topo
 from mininet.util import dumpNodeConnections
 import logging
-import os 
+import os
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger( __name__ )
+logging.basicConfig(filename='./fattree.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class HugeTopo(Topo):
-    logger.debug("Class HugeTopo")
+
+class Fattree(Topo):
+    logger.debug("Class Fattree")
     CoreSwitchList = []
     AggSwitchList = []
     EdgeSwitchList = []
     HostList = []
-    iNUMBER = 0
-    def __init__(self):
-        logger.debug("Class HugeTopo init")
-        iNUMBER = 4
-        
-        self.iNUMBER = iNUMBER
-        self.iCoreLayerSwitch = iNUMBER
-        self.iAggLayerSwitch = iNUMBER * 2
-        self.iEdgeLayerSwitch = iNUMBER * 2
-        self.iHost = self.iEdgeLayerSwitch * 2 
-    
-    
+
+    def __init__(self, k, density):
+        logger.debug("Class Fattree init")
+        self.pod = k
+        self.iCoreLayerSwitch = (k/2)**2
+        self.iAggLayerSwitch = k*k/2
+        self.iEdgeLayerSwitch = k*k/2
+        self.density = density
+        self.iHost = self.iEdgeLayerSwitch * density
+
         #Init Topo
         Topo.__init__(self)
 
-    def createTopo(self):    
-        logger.debug("Start create Core Layer Swich")
+    def createTopo(self):
         self.createCoreLayerSwitch(self.iCoreLayerSwitch)
-        logger.debug("Start create Agg Layer Swich ")
         self.createAggLayerSwitch(self.iAggLayerSwitch)
-        logger.debug("Start create Edge Layer Swich ")
         self.createEdgeLayerSwitch(self.iEdgeLayerSwitch)
-        logger.debug("Start create Host")
         self.createHost(self.iHost)
 
     """
     Create Switch and Host
     """
 
+    def _addSwitch(self, number, level, switch_list):
+        for x in xrange(1, number+1):
+            PREFIX = str(level) + "00"
+            if x >= int(10):
+                PREFIX = str(level) + "0"
+            switch_list.append(self.addSwitch('s' + PREFIX + str(x)))
+
     def createCoreLayerSwitch(self, NUMBER):
         logger.debug("Create Core Layer")
-        for x in range(1, NUMBER+1):
-            PREFIX = "100"
-            if x >= int(10):
-                PREFIX = "10"
-            self.CoreSwitchList.append(self.addSwitch(PREFIX + str(x)))
+        self._addSwitch(NUMBER, 1, self.CoreSwitchList)
 
     def createAggLayerSwitch(self, NUMBER):
-        logger.debug( "Create Agg Layer")
-        for x in range(1, NUMBER+1):
-            PREFIX = "200"
-            if x >= int(10):
-                PREFIX = "20"
-            self.AggSwitchList.append(self.addSwitch(PREFIX + str(x)))
+        logger.debug("Create Agg Layer")
+        self._addSwitch(NUMBER, 2, self.AggSwitchList)
 
     def createEdgeLayerSwitch(self, NUMBER):
         logger.debug("Create Edge Layer")
-        for x in range(1, NUMBER+1):
-            PREFIX = "300"
-            if x >= int(10):
-                PREFIX = "30"
-            self.EdgeSwitchList.append(self.addSwitch(PREFIX + str(x)))
-    
+        self._addSwitch(NUMBER, 3, self.EdgeSwitchList)
+
     def createHost(self, NUMBER):
         logger.debug("Create Host")
-        for x in range(1, NUMBER+1):
-            PREFIX = "400"
+        for x in xrange(1, NUMBER+1):
+            PREFIX = "h00"
             if x >= int(10):
-                PREFIX = "40"
-            self.HostList.append(self.addHost(PREFIX + str(x))) 
+                PREFIX = "h0"
+            elif x >= int(100):
+                PREFIX = "h"
+            self.HostList.append(self.addHost(PREFIX + str(x)))
 
     """
-    Create Link 
+    Add Link
     """
-    def createLink(self):
-        logger.debug("Create Core to Agg")
-        for x in range(0, self.iAggLayerSwitch, 2):
-            self.addLink(self.CoreSwitchList[0], self.AggSwitchList[x], bw=1000, loss=5)
-            self.addLink(self.CoreSwitchList[1], self.AggSwitchList[x], bw=1000, loss=5)
-        for x in range(1, self.iAggLayerSwitch, 2):
-            self.addLink(self.CoreSwitchList[2], self.AggSwitchList[x], bw=1000, loss=5)
-            self.addLink(self.CoreSwitchList[3], self.AggSwitchList[x], bw=1000, loss=5)
-        
-        logger.debug("Create Agg to Edge")
-        for x in range(0, self.iAggLayerSwitch, 2):
-            self.addLink(self.AggSwitchList[x], self.EdgeSwitchList[x], bw=100)
-            self.addLink(self.AggSwitchList[x], self.EdgeSwitchList[x+1], bw=100)
-            self.addLink(self.AggSwitchList[x+1], self.EdgeSwitchList[x], bw=100)
-            self.addLink(self.AggSwitchList[x+1], self.EdgeSwitchList[x+1], bw=100)
+    def createLink(self, bw_c2a=0.2, bw_a2e=0.1, bw_h2a=0.5):
+        logger.debug("Add link Core to Agg.")
+        end = self.pod/2
+        for x in xrange(0, self.iAggLayerSwitch, end):
+            for i in xrange(0, end):
+                for j in xrange(0, end):
+                    self.addLink(
+                        self.CoreSwitchList[i*end+j],
+                        self.AggSwitchList[x+i],
+                        bw=bw_c2a)
 
-        logger.debug("Create Edge to Host")
-        for x in range(0, self.iEdgeLayerSwitch):
-            ## limit = 2 * x + 1 
-            self.addLink(self.EdgeSwitchList[x], self.HostList[2 * x])
-            self.addLink(self.EdgeSwitchList[x], self.HostList[2 * x + 1])
+        logger.debug("Add link Agg to Edge.")
+        for x in xrange(0, self.iAggLayerSwitch, end):
+            for i in xrange(0, end):
+                for j in xrange(0, end):
+                    self.addLink(
+                        self.AggSwitchList[x+i], self.EdgeSwitchList[x+j],
+                        bw=bw_a2e)
 
-def enableSTP():
-    """
-    //HATE: Dirty Code
-    """
-    for x in range(1,5):
-        cmd = "ovs-vsctl set Bridge %s stp_enable=true" % ("100" + str(x))
-        os.system(cmd)
-        print cmd 
+        logger.debug("Add link Edge to Host.")
+        for x in xrange(0, self.iEdgeLayerSwitch):
+            for i in xrange(0, self.density):
+                self.addLink(
+                    self.EdgeSwitchList[x],
+                    self.HostList[self.density * x + i],
+                    bw=bw_h2a)
 
-    for x in range(1, 9):
-        cmd = "ovs-vsctl set Bridge %s stp_enable=true" % ("200" + str(x))
-        os.system(cmd)  
-        print cmd 
-        cmd = "ovs-vsctl set Bridge %s stp_enable=true" % ("300" + str(x))
-        os.system(cmd)
-        print cmd
+    def set_ovs_protocol_13(self,):
+        self._set_ovs_protocol_13(self.CoreSwitchList)
+        self._set_ovs_protocol_13(self.AggSwitchList)
+        self._set_ovs_protocol_13(self.EdgeSwitchList)
+
+    def _set_ovs_protocol_13(self, sw_list):
+            for sw in sw_list:
+                cmd = "sudo ovs-vsctl set bridge %s protocols=OpenFlow13" % sw
+                os.system(cmd)
+
 
 def iperfTest(net, topo):
     logger.debug("Start iperfTEST")
-    h1000, h1015, h1016 = net.get(topo.HostList[0], topo.HostList[14], topo.HostList[15])
-    
-    #iperf Server
-    h1000.popen('iperf -s -u -i 1 > iperf_server_differentPod_result', shell=True)
+    h1000, h1015, h1016 = net.get(
+        topo.HostList[0], topo.HostList[14], topo.HostList[15])
 
     #iperf Server
-    h1015.popen('iperf -s -u -i 1 > iperf_server_samePod_result', shell=True)
+    h1000.popen(
+        'iperf -s -u -i 1 > iperf_server_differentPod_result', shell=True)
+
+    #iperf Server
+    h1015.popen(
+        'iperf -s -u -i 1 > iperf_server_samePod_result', shell=True)
 
     #iperf Client
     h1016.cmdPrint('iperf -c ' + h1000.IP() + ' -u -t 10 -i 1 -b 100m')
     h1016.cmdPrint('iperf -c ' + h1015.IP() + ' -u -t 10 -i 1 -b 100m')
 
+
 def pingTest(net):
     logger.debug("Start Test all network")
     net.pingAll()
 
-def createTopo():
-    logging.debug("LV1 Create HugeTopo")
-    topo = HugeTopo()
-    topo.createTopo() 
-    topo.createLink() 
-    
+
+def createTopo(pod, density, ip="127.0.0.1", port=6633, bw_c2a=0.2, bw_a2e=0.1, bw_h2a=0.05):
+    logging.debug("LV1 Create Fattree")
+    topo = Fattree(pod, density)
+    topo.createTopo()
+    topo.createLink(bw_c2a=bw_c2a, bw_a2e=bw_a2e, bw_h2a=bw_h2a)
+
     logging.debug("LV1 Start Mininet")
-    CONTROLLER_IP = "127.0.0.1"
-    CONTROLLER_PORT = 6633
-    net = Mininet(topo=topo, link=TCLink, controller=None)
-    net.addController( 'controller',controller=RemoteController,ip=CONTROLLER_IP,port=CONTROLLER_PORT)
+    CONTROLLER_IP = ip
+    CONTROLLER_PORT = port
+    net = Mininet(topo=topo, link=TCLink, controller=None, autoSetMacs=True,
+                  autoStaticArp=True)
+    net.addController(
+        'controller', controller=RemoteController,
+        ip=CONTROLLER_IP, port=CONTROLLER_PORT)
     net.start()
 
+    '''
+        Set OVS's protocol as OF13
+    '''
+    topo.set_ovs_protocol_13()
+
     logger.debug("LV1 dumpNode")
-    enableSTP()
-    dumpNodeConnections(net.hosts)
-    
-    pingTest(net)
-    iperfTest(net, topo)
-    
+
+    #dumpNodeConnections(net.hosts)
+    #pingTest(net)
+    #iperfTest(net, topo)
 
     CLI(net)
     net.stop()
@@ -169,4 +171,4 @@ if __name__ == '__main__':
     if os.getuid() != 0:
         logger.debug("You are NOT root")
     elif os.getuid() == 0:
-        createTopo()
+        createTopo(8, 4)
